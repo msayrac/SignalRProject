bd700d957df05ae5251f9cc110ba09d33beb8b89
msayrac <msayracode@gmail.com>
2024-05-14 13:50:42 +0300

    UI page added

---
+++ SignalRWebUI/ViewComponents/UILayoutsComponents/_UILayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.UILayoutsComponents
+{
+    public class _UILayoutFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
