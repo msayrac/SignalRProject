e78d1cd3188bc148f71323b88fdafe318bec1b82
msayrac <msayracode@gmail.com>
2024-05-20 12:53:29 +0300

    BasketAdd added

--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BasketDtos;
 using SignalRWebUI.Dtos.ProductDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
@@ -23,6 +25,29 @@ namespace SignalRWebUI.Controllers
 			var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
 			return View(values);
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddBasket(CreateBasketDto createBasketDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var jsonData = JsonConvert.SerializeObject(createBasketDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+			var responseMessage = await client.PostAsync("https://localhost:7244/api/Basket", stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+
+
+
+
 
 	}
 }
---
+++ SignalRWebUI/Dtos/BasketDtos/CreateBasketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRWebUI.Dtos.BasketDtos
+{
+	public class CreateBasketDto
+	{
+		public decimal Price { get; set; }
+		public int Count { get; set; }
+		public decimal TotalPrice { get; set; }
+		public int ProductID { get; set; }
+		public int MenuTableID { get; set; }
+	}
+}
