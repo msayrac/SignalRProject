c94cc40e6372fe0351cb01a2ff11214aac238733
msayrac <msayracode@gmail.com>
2024-05-28 12:11:27 +0300

    message with SignalR

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -125,6 +125,11 @@ namespace SignalRApi.Hubs
 
 		}
 
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+
 
 
 
