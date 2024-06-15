972ef4b51d60578b9bb60df1ed98252992fda37d
msayrac <msayracode@gmail.com>
2024-06-15 12:41:58 +0300

    QR Code added

---
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+	public class SettingController : Controller
+	{
+		
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
